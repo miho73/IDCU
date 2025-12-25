751785585883d7f26074326f2aadc6a5538da148
miho73 <hyrexfiels@gmail.com>
2025-12-26 01:03:16 +0900

    initial commit

---
+++ include/IDCU.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <iostream>
+#include <Windows.h>
+#include <mmsystem.h>
+#include "spdlog/spdlog.h"
+
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "spdlogd.lib")
+
+using namespace std;
+using namespace spdlog;
---
+++ src/IDCU.cpp
@@ -0,0 +1,16 @@
+﻿#include "IDCU.h"
+
+void PrintBanner() {
+  cout << "IDCU Started";
+}
+
+void SetSpdLog() {
+  set_level(level::info);
+  set_pattern("[%Y-%m-%d %T.%e] [%^%5l%$] %v");
+}
+
+int main() {
+  SetSpdLog();
+
+  info("IDCU Application Starting...");
+}
